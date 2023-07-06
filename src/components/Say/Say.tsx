4a9ae77085449ca352bd62c1d56cb71872db5d20
cantdocpp <cihuyman95@gmail.com>
2023-07-06 15:01:18 +0700

    add say component

--- src/components/Say/Say.tsx
+++ src/components/Say/Say.tsx
@@ -1,32 +1,52 @@
 "use client";
 
 import React from "react";
+import { Howl } from "howler";
 
 import styles from "./say.module.css";
 
 interface Props {
   text: string;
   delay?: number;
+  voice?: any;
 }
 
-export default function Say({ text, delay = 30 }: Props) {
-  const [message, _] = React.useState(text);
+export default function Say({ text, delay = 20, voice }: Props) {
   const [displayed, updateDisplay] = React.useState("");
 
   let animID: NodeJS.Timer;
   React.useEffect(() => {
-    updateDisplay(message.charAt(0));
+    updateDisplay(text.charAt(0));
     animID = setInterval(typeLetter, delay);
+
     return () => {
       updateDisplay("");
       clearInterval(animID);
     };
   }, [text]);
 
+  React.useEffect(() => {
+    if (!voice) {
+      return;
+    }
+    const sound = new Howl({
+      src: [voice],
+      loop: false,
+      volume: 1.0,
+      preload: true,
+    });
+
+    sound.play();
+
+    return () => {
+      sound.stop();
+    };
+  }, [voice]);
+
   const typeLetter = () => {
     updateDisplay((prevText) => {
-      if (message.length <= prevText.length) clearInterval(animID);
-      return prevText.concat(message.charAt(prevText.length));
+      if (text.length <= prevText.length) clearInterval(animID);
+      return prevText.concat(text.charAt(prevText.length));
     });
   };
 
