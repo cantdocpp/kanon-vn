171497f478623447fa2c081e691a380f8f41fe8d
cantdocpp <cihuyman95@gmail.com>
2023-07-06 15:00:25 +0700

    read mp3

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   webpack(config, options) {
     config.module.rules.push({
-      test: /\.mp3$/,
+      test: /\.(mp3|ogg)$/i,
       use: {
         loader: "url-loader",
       },
