8e6becc00be554ac79f54ed12ba71c1e3fefd068
cantdocpp <cihuyman95@gmail.com>
2023-07-07 14:39:52 +0700

    update some type

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,5 +1,5 @@
 export interface Story {
-  showText?: boolean;
+  showText?: boolean | "no-text";
   speaker?: string;
   text?: string;
   spriteLeft?: string;
@@ -10,6 +10,7 @@ export interface Story {
   bgm?: any;
   bgImage?: any;
   voice?: any;
+  animation?: any;
 }
 
 export type Stories = Story[];
